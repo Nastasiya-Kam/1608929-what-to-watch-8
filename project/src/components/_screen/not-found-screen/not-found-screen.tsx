58adbfc62798efce822bf5f279ae1ba4051ecc75
Анастасия <kamanina963@gmail.com>
2021-10-09 10:58:19 +0300

    Создан компонент для 404 страницы

---
+++ project/src/components/_screen/not-found-screen/not-found-screen.tsx
@@ -0,0 +1,45 @@
+import {Link} from 'react-router-dom';
+
+function NotFoundScreen(): JSX.Element {
+  return(
+    <div className="user-page">
+      <header className="page-header user-page__head">
+        <div className="logo">
+          <a href="main.html" className="logo__link">
+            <span className="logo__letter logo__letter--1">W</span>
+            <span className="logo__letter logo__letter--2">T</span>
+            <span className="logo__letter logo__letter--3">W</span>
+          </a>
+        </div>
+      </header>
+
+      <div className="user-page__content">
+        <h1 className="page-title user-page__title">
+          404.
+          <br/>
+          <small>Page not found</small>
+          <br/>
+          <br/>
+          <Link to="/" className="user-block__link">Go to main page</Link>
+        </h1>
+      </div>
+
+
+      <footer className="page-footer">
+        <div className="logo">
+          <a href="main.html" className="logo__link logo__link--light">
+            <span className="logo__letter logo__letter--1">W</span>
+            <span className="logo__letter logo__letter--2">T</span>
+            <span className="logo__letter logo__letter--3">W</span>
+          </a>
+        </div>
+
+        <div className="copyright">
+          <p>© 2019 What to watch Ltd.</p>
+        </div>
+      </footer>
+    </div>
+  );
+}
+
+export default NotFoundScreen;
