c0560c4546267807f74766e0f799c6e7951a77a3
Анастасия <kamanina963@gmail.com>
2021-11-18 22:44:23 +0300

    Настроено отображение сообщений при валидации  ERR
    - ОШИБКА: сообщения опаздывают на один введённый символ при валидации

--- project/src/components/sign-in-message/sign-in-message.tsx
+++
@@ -1,9 +0,0 @@
-function SignInMessage(): JSX.Element {
-  return (
-    <div className="sign-in__message">
-      <p>We can’t recognize this email <br/> and password combination. Please try again.</p>
-    </div>
-  );
-}
-
-export default SignInMessage;
