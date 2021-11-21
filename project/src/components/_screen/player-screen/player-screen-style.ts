ed740c480aa3bbc4c3de2477f2a5b6e68505b0ba
Анастасия <kamanina963@gmail.com>
2021-11-21 23:34:24 +0300

    Реализован ползунок и таймер в плеере

---
+++ project/src/components/_screen/player-screen/player-screen-style.ts
@@ -0,0 +1,12 @@
+import dayjs from 'dayjs';
+import duration from 'dayjs/plugin/duration';
+dayjs.extend(duration);
+
+const SECONDS_IN_MINUTES = 60;
+
+const getTimeFormat = (currentTime: number): string => {
+  const timeFormat = currentTime >= (SECONDS_IN_MINUTES * SECONDS_IN_MINUTES) ? '-HH:mm:ss' : '-mm:ss';
+  return dayjs.duration(currentTime, 'seconds').format(timeFormat);
+};
+
+export {getTimeFormat};
--- project/src/components/_screen/player-screen/player-screen.tsx
+++ project/src/components/_screen/player-screen/player-screen.tsx
@@ -4,32 +4,8 @@ import {FilmId} from '../../../types/films';
 import NotFoundScreen from '../not-found-screen/not-found-screen';
 import {useEffect, useRef, useState} from 'react';
 import {useHistory} from 'react-router';
-import dayjs from 'dayjs';
-import duration from 'dayjs/plugin/duration';
 import {getCurrentFilmById} from '../../../store/film-data/selectors';
-// import LoadingScreen from '../loading-screen/loading-screen';
-dayjs.extend(duration);
-
-const SECONDS_IN_MINUTES = 60;
-
-const getTimeFormat = (currentTime: number) => {
-  const timeFormat = currentTime >= (SECONDS_IN_MINUTES * SECONDS_IN_MINUTES) ? '-HH:mm:ss' : '-mm:ss';
-  return dayjs.duration(currentTime, 'seconds').format(timeFormat);
-};
-
-// const minutesToTime = (duration: number) => {
-//   const hours = Math.floor(duration / (SECONDS_IN_MINUTES * SECONDS_IN_MINUTES));
-//   const divisorForMinutes = duration % (SECONDS_IN_MINUTES * SECONDS_IN_MINUTES);
-//   const minutes = Math.floor(divisorForMinutes / SECONDS_IN_MINUTES);
-//   const divisorForSeconds = divisorForMinutes % SECONDS_IN_MINUTES;
-//   const seconds = Math.ceil(divisorForSeconds);
-
-//   return {
-//     'h': hours,
-//     'm': minutes,
-//     's': seconds,
-//   };
-// };
+import {getTimeFormat} from './player-screen-style';
 
 type Props = {
   currentId: FilmId,
@@ -44,36 +20,38 @@ const mapStateToProps = (state: State, ownProps: Props) => {
 };
 
 const connector = connect(mapStateToProps);
-
 type PropsFromRedux = ConnectedProps<typeof connector>;
+
+const calculateProgress = (totalTime: number, currentTime: number) => {
+  if (!totalTime || !currentTime) {
+    return 0;
+  }
+
+  return currentTime / totalTime * 100;
+};
 
 function PlayerScreen({currentFilm}: PropsFromRedux): JSX.Element {
   const [isPlaying, setIsPlaying] = useState<boolean>(true);
   const [leftTime, setLeftTime] = useState<number>(0);
-  // const [playerTogglerStyle, setPlayerTogglerStyle] = useState<number>(0);
+  const [progress, setProgress] = useState<number>(0);
   const videoRef = useRef<HTMLVideoElement | null>(null);
-
-  // TODO Получаем длину фильма video.duration
-  // todo используем счётчик в useEffect, чтобы шли секунды (убывание на 1)
-  // todo когда фильм на паузе, счётчик должен перестать идти
 
   useEffect(() => {
     if (videoRef.current === null) {
       return;
     }
 
-    setLeftTime(videoRef.current?.duration);
+    setLeftTime(videoRef.current.duration);
   }, [videoRef]);
 
   useEffect(() => {
-    const video: HTMLVideoElement | null = videoRef.current;
-    if (video !== null) {
-      setIsPlaying(!video.paused);
+    if (videoRef.current !== null) {
+      setIsPlaying(!videoRef.current.paused);
     }
   }, [videoRef, currentFilm]);
 
   useEffect(() => {
-    const video = videoRef.current;
+    const video: HTMLVideoElement | null = videoRef.current;
     if (!video) {
       return;
     }
@@ -96,13 +74,19 @@ function PlayerScreen({currentFilm}: PropsFromRedux): JSX.Element {
   }, [videoRef]);
 
   useEffect(() => {
-    const video = videoRef.current;
+    const video: HTMLVideoElement | null = videoRef.current;
     if (!video) {
       return;
     }
 
     const timerId = setInterval(
-      () => setLeftTime((prevLeftTime) => prevLeftTime - 1),
+      () => {
+        const videoDuration = video.duration;
+        const currentTime = video.currentTime;
+        const timeLeft = videoDuration - currentTime;
+        setLeftTime(timeLeft);
+        setProgress(calculateProgress(videoDuration, currentTime));
+      },
       1000,
     );
 
@@ -114,7 +98,6 @@ function PlayerScreen({currentFilm}: PropsFromRedux): JSX.Element {
 
   const onPlay = () => {
     const video: HTMLVideoElement | null = videoRef.current;
-
     if (!video) {
       return;
     }
@@ -133,15 +116,6 @@ function PlayerScreen({currentFilm}: PropsFromRedux): JSX.Element {
   }
 
   const {videoLink} = currentFilm;
-
-  // const durationTime = videoRef?.current?.duration;
-
-  // const playerTogglerStyle = {
-  //   left: `${leftTime / durationTime}%`,
-  // };
-
-  // eslint-disable-next-line no-console
-  console.log(getTimeFormat(leftTime));
 
   return (
     <div className="player">
@@ -165,18 +139,10 @@ function PlayerScreen({currentFilm}: PropsFromRedux): JSX.Element {
       <div className="player__controls">
         <div className="player__controls-row">
           <div className="player__time">
-            <progress className="player__progress" value="30" max="100"></progress>
-            {/* <div className="player__toggler" style={playerTogglerStyle}>Toggler</div> */}
+            <progress className="player__progress" value={progress} max="100"></progress>
+            <div className="player__toggler" style={{left: `${progress}%`}}>Toggler</div>
           </div>
-          {/* //TODO Если продолжительность фильма
-              более часа то -01:45:45
-              менее часа, то -53:00 */}
-          <div className="player__time-value">{getTimeFormat(leftTime)}
-            {/* //TODO магические числа! 60 и 10. Вынести в формулу весь? */}
-            {/* {(currentTime >= 60)
-              ? `-${(timing.h < 10) ? '0' : ''}${timing.h}:${(timing.m < 10) ? '0' : ''}${timing.m}:${(timing.s < 10) ? '0' : ''}${timing.s}`
-              : `-${(timing.m < 10) ? '0' : ''}${timing.m}:${(timing.s < 10) ? '0' : ''}${timing.s}`} */}
-          </div>
+          <div className="player__time-value">{getTimeFormat(leftTime)}</div>
         </div>
 
         <div className="player__controls-row">
